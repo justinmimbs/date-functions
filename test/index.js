b2be7a30add678ef78fd6326d0a824b0d71b0699
Justin Mimbs <justin.mimbs@gmail.com>
2016-02-28 16:26:43 -0500

    Convert indentation to 2 spaces

--- test/index.js
+++ test/index.js
@@ -1,3 +1,2 @@
 const req = require.context('../lib/', true, /\.spec\.js$/);
 req.keys().forEach(req);
- 
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,19 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    entry: {
-        'test/spec': './test/index.js'
-    },
-    output: {
-        path: './',
-        filename: '[name].js',
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-            }
-        ]
-    }
+  entry: {
+    'test/spec': './test/index.js',
+  },
+  output: {
+    path: './',
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+      },
+    ],
+  },
 };
